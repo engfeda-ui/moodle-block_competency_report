da75e2baf99be74b804dd6fef938da2b70c73cab
engfeda-ui <eng.feda@gmail.com>
2026-05-19 15:32:04 +0300

    build: bump plugin version to 2026051900 and dependency local_competency_report

--- version.php
+++ version.php
@@ -25,12 +25,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_competency_report';
-$plugin->version   = 2026051601;
+$plugin->version   = 2026051900;
 $plugin->requires  = 2024042210; // Requires Moodle 4.5.
 $plugin->supported = [405, 500]; // Supported Moodle versions.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v1.0.0';
+$plugin->release   = 'v1.0.1';
 
 $plugin->dependencies = [
-    'local_competency_report' => 2026051601,
+    'local_competency_report' => 2026051900,
 ];
