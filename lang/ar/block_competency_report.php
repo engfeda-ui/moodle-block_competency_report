9c9f8263550a23cddaf85dc17fe0c95c5d0e5284
fody-dev <mahmoud.feda@gmail.com>
2026-07-08 14:57:58 +0300

    Add Arabic translation file

---
+++ lang/ar/block_competency_report.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Arabic language strings for block_competency_report.
+ *
+ * @package   block_competency_report
+ * @copyright 2026 Mahmoud Salem <m.salem@ewa.bh>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'نظرة عامة على تقرير الكفاءات';
+$string['competency_report:addinstance'] = 'إضافة كتلة تقرير الكفاءات جديدة';
+$string['competency_report:myaddinstance'] = 'إضافة كتلة تقرير الكفاءات إلى لوحة معلوماتي';
+$string['course_summary'] = 'تحقق من مستوى تقدم كفاياتك الفنية في هذا المقرر.';
+$string['dashboard_summary'] = 'إنجاز كفاياتك الإجمالي عبر كافة المقررات الدراسية.';
+$string['nodata'] = 'لا تتوفر بيانات تقدم للكفايات حالياً.';
+$string['totalproficient'] = 'إجمالي الكفاءات المتقنة: {$a}';
+$string['totalproficient_label'] = 'الكفاءات المتقنة';
+$string['viewmyreport'] = 'عرض بطاقة التقرير الكاملة';
+
+// Privacy.
+$string['privacy:metadata'] = 'كتلة نظرة عامة على تقارير الكفاءات لا تقوم بحفظ أي بيانات شخصية.';
+$string['privacy:metadata:local_competency_report'] = 'تقوم هذه الكتلة بقراءة بيانات تقدم الجدارات فقط من إضافة تقارير الكفاءات المحلية (local_competency_report).';
