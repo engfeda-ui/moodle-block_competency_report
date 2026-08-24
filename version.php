f52dc1b88f19e77f080191144a7bc712334ef661
engfeda-ui <eng.feda@gmail.com>
2026-08-24 12:17:07 +0300

    chore(repo): v1.4.2 add gitignore, gitattributes and exclude local agents

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_comp_report_ext';
-$plugin->version   = 2026072701;
+$plugin->version   = 2026082400;
 $plugin->requires  = 2024042210; // Requires Moodle 4.5.
 $plugin->supported = [405, 500]; // Supported Moodle versions.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v1.4.1';
+$plugin->release   = 'v1.4.2';
 
 $plugin->dependencies = [];
